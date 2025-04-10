ac4ac67475597c6fb2c974d216dc8cbce1be5d76
Amara Tie <amara.tie@stu.ocsb.ca>
2025-04-10 00:26:59 +0000

    coordinate.py

--- coordinate.py
+++ coordinate.py
@@ -4,13 +4,14 @@
 
 # Date: April 1, 2025
 
-# This is a program that finds out what quadrant each coordinate lies 
+# This is a program that finds out what quadrant each coordinate lies
+
 
 def main():
     # Greeting
     print("Hello! Let's find the quadrant of a coordinate!")
 
-     # Ask user for x and y coordinates
+    # Ask user for x and y coordinates
     x_as_string = input("Enter the x coordinate: ")
     y_as_string = input("Enter the y coordinate: ")
     print("")
@@ -20,20 +21,37 @@ def main():
         x_as_number = int(x_as_string)
         y_as_number = int(y_as_string)
         if x_as_number > 0 and y_as_number > 0:
-            print("The coordinates {}, {} are in quadrant 1.".format(x_as_number, y_as_number))
+            print(
+                "The coordinates {}, {} are in quadrant 1.".format(
+                    x_as_number, y_as_number
+                )
+            )
         elif x_as_number < 0 and y_as_number > 0:
-            print("The coordinates {}, {} are in quadrant 2.".format(x_as_number, y_as_number))
+            print(
+                "The coordinates {}, {} are in quadrant 2.".format(
+                    x_as_number, y_as_number
+                )
+            )
         elif x_as_number < 0 and y_as_number < 0:
-            print("The coordinates {}, {} are in quadrant 3.".format(x_as_number, y_as_number))
+            print(
+                "The coordinates {}, {} are in quadrant 3.".format(
+                    x_as_number, y_as_number
+                )
+            )
         elif x_as_number > 0 and y_as_number < 0:
-            print("The coordinates {}, {} are in quadrant 4.".format(x_as_number, y_as_number))
+            print(
+                "The coordinates {}, {} are in quadrant 4.".format(
+                    x_as_number, y_as_number
+                )
+            )
         else:
-                print("This point doesn't exist")
+            print("This point doesn't exist")
 
-    except :
+    except:
         print("That was not a number.")
 
     print("Thanks for playing!")
+
 
 if __name__ == "__main__":
     main()
